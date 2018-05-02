108f6ee2ae171fbcecbd8f5c4759b473d847db27
Sathveegan <sath49@gmail.com>
2018-05-02 16:44:26 +0530

    Some updated

--- app/src/main/java/com/app/uni/uniapp/ui/MainActivity.java
+++ app/src/main/java/com/app/uni/uniapp/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.app.uni.uniapp.ui;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -7,6 +8,7 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -40,7 +42,8 @@ public class MainActivity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            //super.onBackPressed();
+            BackPressed();
         }
     }
 
@@ -78,7 +81,8 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_home) {
-
+            fragmentTransaction.replace(R.id.fragment_main, new HomeFragment());
+            fragmentTransaction.commit();
         } else if (id == R.id.nav_location) {
             fragmentTransaction.replace(R.id.fragment_main, new LocationFragment());
             fragmentTransaction.commit();
@@ -87,11 +91,38 @@ public class MainActivity extends AppCompatActivity
             fragmentTransaction.commit();
 
         } else if (id == R.id.nav_logout) {
-
+            BackPressed();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    public void BackPressed() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("Exit Application?");
+        alertDialogBuilder
+                .setMessage("Click yes to exit!")
+                .setCancelable(false)
+                .setPositiveButton("Yes",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                moveTaskToBack(true);
+                                android.os.Process.killProcess(android.os.Process.myPid());
+                                System.exit(1);
+                            }
+                        })
+
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
 }
--- app/src/main/java/com/app/uni/uniapp/ui/SemesterRegisterFragment.java
+++ app/src/main/java/com/app/uni/uniapp/ui/SemesterRegisterFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.app.uni.uniapp.R;
@@ -33,7 +34,14 @@ public class SemesterRegisterFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_semester_register, container, false);
 
         Button submit = view.findViewById(R.id.semester_pay);
-        EditText id = view.findViewById(R.id.semester_id);
+        final EditText id = view.findViewById(R.id.semester_id);
+        final EditText name = view.findViewById(R.id.semester_name);
+        final EditText contact = view.findViewById(R.id.semester_contact);
+        final EditText email = view.findViewById(R.id.semester_mail);
+        final EditText year = view.findViewById(R.id.semester_year);
+        final EditText semi = view.findViewById(R.id.semester_semi);
+        final EditText amount = view.findViewById(R.id.semester_amount);
+
 
         FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
         final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -41,8 +49,36 @@ public class SemesterRegisterFragment extends Fragment {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                fragmentTransaction.replace(R.id.fragment_main, new CardFragment());
-                fragmentTransaction.addToBackStack(null).commit();
+
+                if (id.getText().toString().trim().equals("")) {
+                    id.setError("ID required!");
+                }
+                if (name.getText().toString().trim().equals("")) {
+                    name.setError("Name required!");
+                }
+                if (contact.getText().toString().trim().equals("")) {
+                    contact.setError("Contact No required!");
+                }
+                if (email.getText().toString().trim().equals("")) {
+                    email.setError("Email required!");
+                }
+                if (year.getText().toString().trim().equals("")) {
+                    year.setError("Year required!");
+                }
+
+                if (semi.getText().toString().trim().equals("")) {
+                    semi.setError("Semi required!");
+                }
+
+                if (amount.getText().toString().trim().equals("")) {
+                    amount.setError("Amount required!");
+                }
+
+                else{
+                    fragmentTransaction.replace(R.id.fragment_main, new CardFragment());
+                    fragmentTransaction.addToBackStack(null).commit();
+                }
+
             }
         });
 
